dca443485ae1610660a1340cff6caa8d9022e67e
eurides <eurides@eurides-VirtualBox>
2019-03-07 20:53:46 -0300

    Doc: Adicionar estrutura inicial com spring boot

---
+++ backend-pizza-api/src/main/java/br/com/eurides/example/backend/pizza/api/Application.java
@@ -0,0 +1,13 @@
+package br.com.eurides.example.backend.pizza.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ backend-pizza-util/src/main/java/br/com/eurides/example/backend/pizza/util/BackendPizzaConstant.java
@@ -0,0 +1,8 @@
+package br.com.eurides.example.backend.pizza.util;
+
+public class BackendPizzaConstant {
+
+	private BackendPizzaConstant() {
+	}
+
+}
